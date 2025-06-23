340626e1babf46db528486ba8cb1854a771d6c00
21arkumar <81228514+21arkumar@users.noreply.github.com>
2025-06-23 18:07:46 +0530

    Time: 24 ms (98.11%), Space: 51.5 MB (58.78%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        int len = nums.length;
+
         Arrays.sort(nums);
 
-        for(int i = 0; i < len; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            
-            int n1 = -1*nums[i];
-
-            int j = i + 1;
-            int k = len-1;
+        for(int i = 0; i < nums.length-2; i++){
+            if(nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int j = i+1;
+            int k = nums.length-1;
 
             while(j < k){
                 int total = nums[i] + nums[j] + nums[k];
 
-                if (total > 0) {
+                if(total > 0){
                     k--;
-                } else if (total < 0) {
+                }else if(total < 0){
                     j++;
-                } else {
+                }else{
                     ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
+                    while(nums[j] == nums[j-1] && j < k){
                         j++;
                     }
                 }
