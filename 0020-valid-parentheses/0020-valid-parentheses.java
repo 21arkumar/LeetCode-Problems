3b8fc48ee73f4133fedeaeceb596b6d7cff9186c
21arkumar <81228514+21arkumar@users.noreply.github.com>
2025-07-08 23:54:11 +0530

    Time: 2 ms (96.79%), Space: 41.8 MB (65.71%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+                continue;
+            }
+            if(!st.isEmpty()){
+                if((st.peek() == '(' && ch == ')') || (st.peek() == '[' && ch == ']')  || (st.peek() == '{' && ch == '}') ){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+        if(!st.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+}
