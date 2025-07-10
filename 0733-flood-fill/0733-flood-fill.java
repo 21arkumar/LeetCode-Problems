aa1aab75513a3f0d9ef02d0c1014a8f8c5689576
21arkumar <81228514+21arkumar@users.noreply.github.com>
2025-07-11 03:56:48 +0530

    Time: 1 ms (46.69%), Space: 45.4 MB (10.56%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,35 @@
+class Pair{
+    int x, y;
+    Pair(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(sr, sc));
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                Pair pair = q.poll();
+                
+                if(image[pair.x][pair.y] == color) continue;
+
+                for(int[] arr : dir){
+                    int row = pair.x + arr[0];
+                    int col = pair.y + arr[1];
+                    if(row < 0 || col < 0 || row >= image.length || col >= image[0].length || image[row][col] != image[pair.x][pair.y] || image[row][col] == color) continue;
+                    
+                    q.add(new Pair(row, col));
+                }
+                image[pair.x][pair.y] = color;
+            }
+        }
+
+        return image;
+    }
+}
