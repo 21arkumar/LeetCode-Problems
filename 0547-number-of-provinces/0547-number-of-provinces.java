32bdd9c14d4d82377b4f3b1577507bce08a13c56
21arkumar <81228514+21arkumar@users.noreply.github.com>
2025-09-28 00:10:56 +0530

    Time: 18 ms (5.06%), Space: 48.2 MB (21.32%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,22 +1,64 @@
-class Solution {
-    public void dfs(int[][] isConnected, boolean[] vis, int node){
-        vis[node] = true;
-        for(int i = 0; i < isConnected[node].length; i++){
-            if(vis[i] == true) continue;
-            if(isConnected[node][i] == 1 && vis[i] != true){
-                dfs(isConnected, vis, i);
+class DSU{
+    int[] parent;
+    int[] rank; 
+
+    DSU(int n){
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        for(int i = 0; i < n + 1; i++){
+            parent[i] = i;
+            rank[i] = 0;        //rank intially 0 for all nodes
+            
+        }
+    }
+
+    public int findParent(int u){
+        if(parent[u] == u){
+            return u;
+        }
+        
+        return parent[u] = findParent(parent[u]);
+    
+    }
+
+    public void unionByRank(int u, int v){
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if(pu != pv){
+            if(rank[pu] > rank[pv]){
+                parent[pv] = pu;
+            }else if(rank[pu] < rank[pv]){
+                parent[pu] = pv;
+            }else{
+                parent[pv] = pu;
+                rank[pu]++;
             }
         }
     }
+}
+
+class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int province = 0;
-        boolean[] vis = new boolean[isConnected.length];
-        for(int i = 0; i < isConnected.length; i++){
-            if(vis[i] == true) continue;
-            
-            province++;
-            dfs(isConnected, vis, i);
+        int n = isConnected.length;
+        DSU dsu = new DSU(n);
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(isConnected[i][i] == isConnected[i][j]){
+                    dsu.unionByRank(i+1, j+1);
+                }
+            }
         }
-        return province;
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int i = 1; i <= n; i++){
+            int val = dsu.findParent(i);
+            System.out.println("Parent of " + i + " = " + val);
+            set.add(val);
+        }
+
+        return set.size();
     }
 }
