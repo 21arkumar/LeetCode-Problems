482f46740bdd9a3d1016d307628eaded5031b7f3
21arkumar <81228514+21arkumar@users.noreply.github.com>
2025-10-15 21:34:59 +0530

    Time: 0 ms (100%), Space: 41 MB (26.98%) - LeetHub

--- 1674-minimum-operations-to-make-array-equal/1674-minimum-operations-to-make-array-equal.java
+++ 1674-minimum-operations-to-make-array-equal/1674-minimum-operations-to-make-array-equal.java
@@ -1,11 +1,5 @@
 class Solution {
     public int minOperations(int n) {
-        int ans = 0;
-
-        for(int i = 1; i < n; i+=2){
-            ans += n - i;
-        }
-
-        return ans;
+        return n * n / 4;
     }
 }
